a787041d0eb62f94dbfcbb52d747159f6de59343
chandana g <chandana g@LAPTOP-VER72DBN>
2024-02-29 22:04:54 +0530

    diagonal commit

---
+++ src/w7b/Diagonalofmatrix.java
@@ -0,0 +1,36 @@
+package w7b;
+import java.util.Scanner;
+class Diagonalofmatrix
+{
+	public static void main(String args[])
+	{
+		int matrix[][]=new int[3][3];
+		Scanner inScanner=new Scanner(System.in);
+		System.out.println("Enter the matrix elements one by one");
+		for(int i=0;i<3;i++)	
+		{
+			for(int j=0;j<3;j++)
+			{
+				matrix[i][j]=inScanner.nextInt();
+			}
+		}
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(i==j)
+				{
+					System.out.print(matrix[i][j]+"");
+					
+				}
+				else
+				{
+					System.out.print("*");
+					
+				}
+			}
+		System.out.println("\t");
+		}
+		inScanner.close();
+	}
+}
